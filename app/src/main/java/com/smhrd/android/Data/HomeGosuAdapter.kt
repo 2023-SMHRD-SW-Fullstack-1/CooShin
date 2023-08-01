e8fdddd118cb360cfcbb44098f1ca24eb1df145d
이유영 <dbdud31206@naver.com>
2023-08-01 12:53:55 +0900

    Adapter commit

---
+++ app/src/main/java/com/smhrd/android/Data/HomeGosuAdapter.kt
@@ -0,0 +1,31 @@
+package com.smhrd.android.Data
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.smhrd.android.R
+
+class HomeGosuAdapter (var datas : ArrayList<MemberVO>, var context: Context) : RecyclerView.Adapter<HomeGosuViewHolder>() {
+    //View Holder 생성
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeGosuViewHolder {
+        return HomeGosuViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.language_item_view, parent, false)
+        )
+    }
+    //가지고 오는 아이템 수
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+    //아이템을 View Holder에 바인딩
+    override fun onBindViewHolder(holder: HomeGosuViewHolder, position: Int) {
+       val member : MemberVO = datas[position]
+
+        holder.itemView.setOnClickListener(){
+
+        }
+
+    }
+
+
+}
