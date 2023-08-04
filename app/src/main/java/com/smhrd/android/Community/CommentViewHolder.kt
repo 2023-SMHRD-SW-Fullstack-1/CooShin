1eb91bb22b2082959e08df1e893ed38c9028e5ce
천현민 <poppy3672@naver.com>
2023-08-04 14:15:59 +0900

    commit

--- app/src/main/java/com/smhrd/android/Community/CommentViewHolder.kt
+++ app/src/main/java/com/smhrd/android/Community/CommentViewHolder.kt
@@ -9,11 +9,11 @@ class CommentViewHolder(var itemView : View) : RecyclerView.ViewHolder(itemView)
     //Content List
     var tv_writer : TextView
     var tv_comment : TextView
-    var tv_date : TextView
+//    var tv_date : TextView
 
     init{
         tv_writer = itemView.findViewById(R.id.tv_writer)
         tv_comment = itemView.findViewById(R.id.tv_comment)
-        tv_date = itemView.findViewById(R.id.tv_date)
+//        tv_date = itemView.findViewById(R.id.tv_date)
     }
 }
