d1cb70bb7305c428e9a6dfaf48947b17e6d8d368
천현민 <poppy3672@naver.com>
2023-08-04 09:10:15 +0900

    commit

--- app/src/main/java/com/smhrd/android/Community/CommunityViewHolder.kt
+++ app/src/main/java/com/smhrd/android/Community/CommunityViewHolder.kt
@@ -2,6 +2,7 @@ package com.smhrd.android.Community
 
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.smhrd.android.R
@@ -10,19 +11,32 @@ class CommunityViewHolder(var itemView : View) : RecyclerView.ViewHolder(itemVie
 
     //Community List
     var c_title : TextView
-    var c_writer : TextView
-    var c_date : TextView
+//    var c_writer : TextView
+//    var c_date : TextView
     var c_Likes : TextView
-    var c_Views : TextView
+//    var c_Views : TextView
+    var c_img : ImageView
 
 
     init{
         c_title = itemView.findViewById(R.id.c_title)
-        c_writer = itemView.findViewById(R.id.c_writer)
-        c_date = itemView.findViewById(R.id.c_date)
+//        c_writer = itemView.findViewById(R.id.c_writer)
+//        c_date = itemView.findViewById(R.id.c_date)
         c_Likes = itemView.findViewById(R.id.c_Likes)
-        c_Views = itemView.findViewById(R.id.c_Views)
+//        c_Views = itemView.findViewById(R.id.c_Views)
+        c_img = itemView.findViewById(R.id.c_img)
 
+//        board_item.xml
+//
+//        1. viewHolder에 c_img변수 추가
+//        c_writer,c_date,c_Views 변수삭제
+//
+//        2. CommunityFragment에
+//        rc.layoutManager = GridLayoutManager(context, 2)
+//        var adapter = CommunityAdapter(requireContext(), data )
+//        rc.adapter = adapter 추가
+//
+//                3. CommunityAdapter에서 c_writer,c_date,c_Views 삭제
 
     }
 
